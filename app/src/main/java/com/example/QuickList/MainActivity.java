944bbe248b6b3f83c8e593d59dd68e5d7017263d
kevin <kevin.stamenkovski@gmail.com>
2024-04-27 17:44:45 +0200

    image

--- app/src/main/java/com/example/QuickList/MainActivity.java
+++ app/src/main/java/com/example/QuickList/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.QuickList;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button button_login;
