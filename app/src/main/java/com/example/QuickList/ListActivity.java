034cbd3bceaaadb8c051f102849f476b98722b52
z_FedeXx <fedegabu@gmail.com>
2024-04-28 17:23:22 +0200

    updated ListActivity

--- app/src/main/java/com/example/QuickList/ListActivity.java
+++ app/src/main/java/com/example/QuickList/ListActivity.java
@@ -3,12 +3,22 @@ package com.example.QuickList;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ListActivity extends AppCompatActivity {
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(v -> {
+//            Intent intent = new Intent(ListActivity.this, HomePage.class);
+            finish();
+        });
+        Button backButton;
     }
 }
