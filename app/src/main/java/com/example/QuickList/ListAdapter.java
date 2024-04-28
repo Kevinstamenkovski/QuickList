cf84254131989cc97152d1e5e2e1a2b811386875
z_FedeXx <fedegabu@gmail.com>
2024-04-28 18:05:00 +0200

    updates

--- app/src/main/java/com/example/QuickList/ListAdapter.java
+++ app/src/main/java/com/example/QuickList/ListAdapter.java
@@ -2,10 +2,13 @@ package com.example.QuickList;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -44,16 +47,17 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView title;
         TextView number;
+        LinearLayout layout;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.tvListName);
             number = itemView.findViewById(R.id.tvNumerItems);
+            layout = itemView.findViewById(R.id.layout);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
+            layout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    HomeActivity homeActivity = new HomeActivity();
-                    homeActivity.call();
+                    Log.d("LIST", "click");
                 }
             });
         }
