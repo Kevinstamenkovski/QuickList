a95c732953e4ea78ad7021d51a85cb3471226c4b
z_FedeXx <fedegabu@gmail.com>
2024-05-04 21:56:11 +0200

    updates

--- app/src/main/java/com/example/QuickList/ListActivity.java
+++ app/src/main/java/com/example/QuickList/ListActivity.java
@@ -5,24 +5,25 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListActivity extends AppCompatActivity implements ProductInterface {
-    Button backButton, btnAddProduct, btnDialogAddProduct;
+    Button backButton, btnAddProduct, btnDialogAddProduct, btnDialogEditProduct;
     ProductAdapter adapter;
     RecyclerView recyclerView;
     List<String> product_names;
-    List<Integer> prod_amounts;
-    Dialog dialog;
-    EditText etDialogProductName;
+    List<Integer> product_amounts;
+    Dialog dialogAddItem, dialogEditItem;
+    EditText etDialogProductName, etDialogEditProductName, etDialogEditProductAmount;
+    TextView tvDialogEditProductName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,29 +38,38 @@ public class ListActivity extends AppCompatActivity implements ProductInterface 
         });
 
         product_names = new ArrayList<>();
-        prod_amounts = new ArrayList<>();
-//        prod_images = new ArrayList<>();
+        product_amounts = new ArrayList<>();
 
         product_names.add("Ciao");
         product_names.add("Ciao");
         product_names.add("Ciao");
 
-        prod_amounts.add(1);
-        prod_amounts.add(1);
-        prod_amounts.add(1);
+        product_amounts.add(1);
+        product_amounts.add(1);
+        product_amounts.add(1);
 
         recyclerView = findViewById(R.id.rvProducts);
         btnAddProduct = findViewById(R.id.btnAddProduct);
 
-        dialog = new Dialog(ListActivity.this);
-        dialog.setContentView(R.layout.dialog_add_list);
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.setCancelable(false);
+        dialogAddItem = new Dialog(ListActivity.this);
+        dialogAddItem.setContentView(R.layout.dialog_add_product);
+        dialogAddItem.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialogAddItem.setCancelable(false);
 
-        btnDialogAddProduct = dialog.findViewById(R.id.btnDialogAddList);
-        etDialogProductName = dialog.findViewById(R.id.etDialogListName);
+        btnDialogAddProduct = dialogAddItem.findViewById(R.id.btnDialogAddProduct);
+        etDialogProductName = dialogAddItem.findViewById(R.id.etDialogAddProductName);
 
-        adapter = new ProductAdapter(this, product_names, prod_amounts, this);
+        dialogEditItem = new Dialog(ListActivity.this);
+        dialogEditItem.setContentView(R.layout.dialog_product_info);
+        dialogEditItem.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialogEditItem.setCancelable(false);
+
+        etDialogEditProductName = dialogEditItem.findViewById(R.id.etDialogEditProductName);
+        etDialogEditProductAmount = dialogEditItem.findViewById(R.id.etDialogEditProductAmount);
+        tvDialogEditProductName = dialogEditItem.findViewById(R.id.tvDialogEditProductName);
+        btnDialogEditProduct = dialogEditItem.findViewById(R.id.btnDialogEditProduct);
+
+        adapter = new ProductAdapter(this, product_names, product_amounts, this);
 
         //Prendo tutti i valori dal database e li metto in una array list
 
@@ -70,7 +80,7 @@ public class ListActivity extends AppCompatActivity implements ProductInterface 
         btnAddProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog.show();
+                dialogAddItem.show();
             }
         });
 
@@ -80,21 +90,34 @@ public class ListActivity extends AppCompatActivity implements ProductInterface 
                 String name = etDialogProductName.getText().toString();
                 etDialogProductName.setText("");
                 product_names.add(name);
-                prod_amounts.add(0);
+                product_amounts.add(0);
                 adapter.notifyDataSetChanged();
-
-                dialog.dismiss();
+                dialogAddItem.dismiss();
             }
         });
     }
 
     @Override
     public void onItemClick(int position) {
-
+        tvDialogEditProductName.setText(product_names.get(position));
+        etDialogEditProductName.setText(product_names.get(position));
+        etDialogEditProductAmount.setText(String.valueOf(product_amounts.get(position)));
+        dialogEditItem.show();
+        btnDialogEditProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                product_names.set(position, etDialogEditProductName.getText().toString());
+                product_amounts.set(position, Integer.parseInt(etDialogEditProductAmount.getText().toString()));
+                adapter.notifyDataSetChanged();
+                dialogEditItem.dismiss();
+            }
+        });
     }
 
     @Override
     public void onButtonClick(int position) {
-
+        product_names.remove(position);
+        product_amounts.remove(position);
+        adapter.notifyDataSetChanged();
     }
 }
